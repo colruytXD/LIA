333e809ab8c5fd9c7e8ca45691a5b15bf025731d
tom willaert <tom willaert>
2016-07-12 14:39:49 +0200

    Initial commit : Setting up

---
+++ Assets/Scripts/Master scripts/GameManager_Master.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager_Master : MonoBehaviour {
+
+	void OnEnable() 
+	{
+		SetInitialReferences();
+	}
+
+	void OnDisable() 
+	{
+
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	void SetInitialReferences() 
+	{
+
+	}
+}
